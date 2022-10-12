d092a1020c4d92cf1d376c2e5e191785af8d7f07
Yvan Sugira <phoenixnavyn7@gmail.com>
2022-10-12 09:22:20 +0200

    Finalising the layout

--- pro-reckonr/src/App.js
+++ pro-reckonr/src/App.js
@@ -6,6 +6,7 @@ import Divide from "./public/divide.svg";
 import Sub from "./public/minus.svg";
 import Mult from "./public/multiply.svg";
 import Equals from "./public/equals.svg";
+import Dot from "./public/dot.svg";
 
 function App() {
   return (
@@ -18,13 +19,19 @@ function App() {
 class Body extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      answer: 0,
+      input: "0",
+    };
   }
   render() {
     return (
       <>
         <p className="header">PRO~RECKONR</p>
-        <section className="screen"></section>
+        <div className="screen">
+          <p>{this.state.answer}</p>
+          <input value={this.state.input} disabled />
+        </div>
         <MenuRow />
         <div className="body-reck">
           <div className="body-container">
@@ -39,7 +46,7 @@ class Body extends React.Component {
   }
 }
 
-class MenuRow extends React.Component {
+class MenuRow extends Body {
   constructor() {
     super();
     this.state = {};
@@ -60,11 +67,29 @@ class Nums extends React.Component {
   constructor() {
     super();
     this.state = {};
+    this.handleNumClick = this.handleNumClick.bind(this);
   }
+  handleNumClick = (e) => {
+    let inputChange = e.target.value;
+
+    this.setState({
+      input: inputChange,
+    });
+  };
+
   render() {
     let labels = [7, 8, 9, 4, 5, 6, 1, 2, 3];
     let nums = labels.map((label) => {
-      return <button className={`num`}>{label}</button>;
+      return (
+        <button
+          className={`num`}
+          onClick={(e) => {
+            this.handleNumClick(e);
+          }}
+        >
+          {label}
+        </button>
+      );
     });
     return <div className="nums">{nums}</div>;
   }
@@ -78,14 +103,14 @@ class LeftPane extends React.Component {
   render() {
     return (
       <div className="operations">
-        <button className="opBtn">
-          <img src={Add} alt="" />
+        <button className="opBtn add">
+          <img src={Mult} alt="" />
         </button>
         <button className="opBtn sub">
-          <img src={Sub} alt="" />i
+          <img src={Sub} alt="" />
         </button>
         <button className="opBtn divide">
-          <img src={Divide} alt="" />
+          <img src={Add} alt="" />
         </button>
         <button className="opBtn equals">
           <img src={Equals} alt="" />
@@ -105,10 +130,10 @@ class TopRow extends React.Component {
       <div className="topRow">
         <button className="opBtn ac">AC</button>
         <button className="opBtn del">
-          <img src={Del} alt="" />i
+          <img src={Del} alt="" />
         </button>
         <button className="opBtn mult">
-          <img src={Mult} alt="" />
+          <img src={Divide} alt="" />
         </button>
       </div>
     );
@@ -123,12 +148,9 @@ class BottomRow extends React.Component {
   render() {
     return (
       <div className="bottomRow">
-        <button className="opBtn ac">AC</button>
-        <button className="opBtn del">
-          <img src={Del} alt="" />i
-        </button>
-        <button className="opBtn mult">
-          <img src={Mult} alt="" />
+        <button className="opBtn zero">0</button>
+        <button className="opBtn dot">
+          <img src={Dot} alt="" />
         </button>
       </div>
     );
